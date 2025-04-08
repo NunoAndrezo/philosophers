b80e34bd9f92f4b3bddd539ba720d3c2d6b3edaa
nuno <nunoandrezo97@gmail.com>
2025-04-08 21:59:02 +0100

    having problems with odd number of philos

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -2,7 +2,8 @@
 
 static void	ft_eat(t_philo *philo);
 static void	ft_sleep(t_philo *philo);
-static void	ft_think(t_philo *philo);
+static void	ft_think(t_philo *philo, bool did_routine_start);
+static void	make_a_mf_wait(t_philo *philo);
 
 void	*routine(void *philoso)
 {
@@ -13,6 +14,8 @@ void	*routine(void *philoso)
 		usleep(100);
 	change_long(&philo->philo_mutex, &philo->time_last_eat, get_time(MILISECONDS));
 	increase_long(&philo->table->table_mutex, &philo->table->num_threads_running);
+	
+	make_a_mf_wait(philo);
 	while (check_bool(&philo->table->table_mutex, &philo->table->running) == true)
 	{
 		if (check_bool(&philo->philo_mutex, &philo->full) == true)
@@ -31,7 +34,7 @@ void	*routine(void *philoso)
 			print_mutex(philo, DEAD);
 			return (NULL);
 		}
-		ft_think(philo);
+		ft_think(philo, true);
 	}
 	return (NULL);
 }
@@ -59,7 +62,35 @@ static void	ft_sleep(t_philo *philo)
 	ft_usleep(philo->table->time_to_sleep, philo->table);
 }
 
-static void	ft_think(t_philo *philo)
+static void	ft_think(t_philo *philo, bool did_routine_start)
 {
-	print_mutex(philo, THINKING);
+	long	t_to_sleep;
+	long	t_to_eat;
+	long	t_to_think;
+
+	if (did_routine_start == true)
+		print_mutex(philo, THINKING);
+	if (philo->table->num_of_philos % 2 == 0)
+		return ;
+	printf("philos are not even, so we need to think a bit more\n");
+	t_to_sleep = philo->table->time_to_sleep;
+	t_to_eat = philo->table->time_to_eat;
+	t_to_think = ((t_to_eat * 2) - t_to_sleep);
+	if (t_to_think < 0)
+		t_to_think = 0;
+	ft_usleep((t_to_think * 0.42), philo->table);
+}
+
+static void	make_a_mf_wait(t_philo *philo)
+{
+	if (philo->table->num_of_philos % 2 == 0)
+	{
+		if (philo->id % 2 == 0)
+				ft_usleep(30000, philo->table); // 30miliseconds, half the minimum 60mili
+	}
+	else
+	{
+		if (philo->id % 2 != 0)
+			ft_think(philo, false);
+	}
 }
